0b38c4826e8e9b648bea57bb2971ab2f01ff4115
DISSIDIA-986 <121951255@qq.com>
2015-11-11 11:04:38 +0800

    灵活的静态工厂方法构成了服务提供者框架(Service Provider Framework)的基础  服务访问API一般允许但是不要求客户端指定某种选择提供者的条件。如果没有这样的规定，API就会返回默认实现的一个实例。

--- effectivejava/src/main/java/org/effectivejava/examples/chapter02/item01/Services.java
+++ effectivejava/src/main/java/org/effectivejava/examples/chapter02/item01/Services.java
@@ -5,7 +5,12 @@ package org.effectivejava.examples.chapter02.item01;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
+/**
+ * 灵活的静态工厂方法构成了服务提供者框架(Service Provider Framework)的基础
+ * 服务访问API一般允许但是不要求客户端指定某种选择提供者的条件。如果没有这样的规定，API就会返回默认实现的一个实例。
+ * @author dissidia986
+ *
+ */
 public class Services {
 	private Services() {
 	} // Prevents instantiation (Item 4)
